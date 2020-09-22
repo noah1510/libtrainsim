01c0e9fab2efcd9335ff6969590220a31038df22
noasakurajin <noasakurajin@web.de>
2020-09-22 16:25:53 +0200

    implmented more getter functions, documentation

--- video/include/video.hpp
+++ video/include/video.hpp
@@ -2,7 +2,11 @@
 #include <string>
 #include <filesystem>
 
-#include "opencv2/opencv.hpp"
+#if __has_include("opencv2/opencv.hpp")
+    #include "opencv2/opencv.hpp"
+#elif __has_include("opencv4/opencv2/opencv.hpp")
+    #include "opencv4/opencv2/opencv.hpp"
+#endif
 
 namespace libtrainsim {
     /**
@@ -17,6 +21,10 @@ namespace libtrainsim {
              */
             video(void);
 
+            /**
+             * @brief Destroy the video object, on destruction everything will be reset.
+             * 
+             */
             ~video(){
                 reset();
             }
@@ -54,11 +62,32 @@ namespace libtrainsim {
              */
             std::filesystem::path loadedFile;
 
+            /**
+             * @brief This variable saves the state of the video capture device aka the video that is rendered.
+             * 
+             */
             std::unique_ptr<cv::VideoCapture> videoCap;
 
+            ///The implementation for the getFPS method
             double getFPS_impl();
 
+            ///The implementation for the getNextFrame method
             const cv::Mat getNextFrame_impl(); 
+
+            ///The implementation for the getPosMsec method
+            double getPosMsec_impl();
+
+            ///The implementation for the getPosFrames method
+            double getPosFrames_impl();
+
+            ///The implementation for the getWidth method
+            double getWidth_impl();
+
+            ///The implementation for the getHight method
+            double getHight_impl();
+
+            ///The implementation for the getFrameCount method
+            double getFrameCount_impl();
 
         public:
             /**
@@ -80,10 +109,67 @@ namespace libtrainsim {
                 return getInstance().load_impl(uri);
             }
 
+            /**
+             * @brief get the framerate of the video file
+             * 
+             * @return double 
+             */
             static double getFPS(){
                 return getInstance().getFPS_impl();
             }
 
+            /**
+             * @brief Get the current Position in the file in milliseconds
+             * 
+             * @return double 
+             */
+            static double getPosMsec(){
+                return getInstance().getPosMsec_impl();
+            }
+
+            /**
+             * @brief Get the current Position in the file in frames
+             * 
+             * @return double 
+             */
+            static double getPosFrames(){
+                return getInstance().getPosFrames_impl();
+            }
+
+            /**
+             * @brief Get the Width of the video in pixels
+             * 
+             * @return double 
+             */
+            static double getWidth(){
+                return getInstance().getWidth_impl();
+            }
+
+            /**
+             * @brief Get the Height of the video in pixels
+             * 
+             * @return double 
+             */
+            static double getHight(){
+                return getInstance().getHight_impl();
+            }
+
+            /**
+             * @brief Get the total number of frame the video has.
+             * 
+             * @return double 
+             */
+            static double getFrameCount(){
+                return getInstance().getFrameCount_impl();
+            }
+
+            /**
+             * @brief Retrieve the next frame to display it. 
+             * If no video is loaded or there is no new frame, an empty frame will be returned.
+             * You should check the returned frame with the method .empty(), which will return true if the frame is empty.
+             * 
+             * @return const cv::Mat The next frame of the video
+             */
             static const cv::Mat getNextFrame(){
                 return getInstance().getNextFrame_impl();
             }
--- video/src/video.cpp
+++ video/src/video.cpp
@@ -25,14 +25,6 @@ bool libtrainsim::video::load_impl(const std::filesystem::path& uri){
     return true;
 }
 
-double libtrainsim::video::getFPS_impl(){
-    if(videoCap->isOpened()){
-        return videoCap->get(cv::CAP_PROP_FPS);
-    }
-
-    return 0.0f;
-}
-
 const cv::Mat libtrainsim::video::getNextFrame_impl(){
     cv::Mat frame;
     auto status = videoCap->read(frame);
@@ -44,4 +36,52 @@ const cv::Mat libtrainsim::video::getNextFrame_impl(){
     return frame.clone();
 }
 
+
+double libtrainsim::video::getFPS_impl(){
+    if(videoCap->isOpened()){
+        return videoCap->get(cv::CAP_PROP_FPS);
+    }
+
+    return 0.0f;
+}
+
+double libtrainsim::video::getPosMsec_impl(){
+    if(videoCap->isOpened()){
+        return videoCap->get(cv::CAP_PROP_POS_MSEC);
+    }
+
+    return 0.0f;
+}
+
+double libtrainsim::video::getPosFrames_impl(){
+    if(videoCap->isOpened()){
+        return videoCap->get(cv::CAP_PROP_POS_FRAMES);
+    }
+
+    return 0.0f;
+}
+
+double libtrainsim::video::getWidth_impl(){
+    if(videoCap->isOpened()){
+        return videoCap->get(cv::CAP_PROP_FRAME_WIDTH);
+    }
+
+    return 0.0f;
+}
+
+double libtrainsim::video::getHight_impl(){
+    if(videoCap->isOpened()){
+        return videoCap->get(cv::CAP_PROP_FRAME_HEIGHT);
+    }
+
+    return 0.0f;
+ }
+
+ double libtrainsim::video::getFrameCount_impl(){
+    if(videoCap->isOpened()){
+        return videoCap->get(cv::CAP_PROP_FRAME_COUNT);
+    }
+
+    return 0.0f;
+ }
   
