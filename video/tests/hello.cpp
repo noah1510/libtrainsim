c52bc4d30940a0f642bd6b2ee266a43e8361255e
Noah Kirschmann <noah.kirschmann@mnd.thm.de>
2021-05-13 11:24:21 +0000

    Update video/tests/hello.cpp

--- video/tests/hello.cpp
+++ video/tests/hello.cpp
@@ -3,5 +3,6 @@
 #include "video.hpp"
 
 TEST_CASE( "Checking if hello method works", "[vector]" ) {
-    REQUIRE(libtrainsim::video::hello().compare("Hello from the video singleton") == 0);
+    const auto hello = libtrainsim::video::hello();
+    REQUIRE(hello.compare("Hello from the video singleton") == 0);
 };
