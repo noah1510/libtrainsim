ac2892df56925642110389c2eedc9a0c6d4d99ab
noasakurajin <noasakurajin@web.de>
2021-06-28 20:56:45 +0200

    raising version to 0.8.0

--- core/include/types.hpp
+++ core/include/types.hpp
@@ -148,7 +148,7 @@ namespace libtrainsim {
          * @brief the current version of the libtrainsim
          *
          */
-        const version lib_version(0,7,0);
+        const version lib_version(0,8,0);
 
         /**
          * @brief the current version of the json formats
