38e64ce76f64477a39c0b913db16880b1669661a
noasakurajin <dev@noasakurajin.de>
2022-07-20 14:07:36 +0200

    refectored ffmpegFrame into Frame

--- video/include/frame.hpp
+++ video/include/frame.hpp
@@ -1,42 +1,75 @@
 #pragma once
 
+#if __has_include("libavcodec/avcodec.h") && __has_include("libavutil/imgutils.h") && __has_include("libavformat/avformat.h") && __has_include("libswscale/swscale.h")
+    extern "C"{
+        #include <libavcodec/avcodec.h>
+        #include <libavutil/imgutils.h>
+        #include <libavformat/avformat.h>
+        #include <libswscale/swscale.h>
+    }
+#else 
+    #error "Cannot include ffmpeg"
+#endif
+
 #include <iostream>
 
 namespace libtrainsim {
+    namespace Video{
+        /**
+        * @brief an abstraction on whatever backend is used for handling frames.
+        * 
+        */
+        class Frame{
+            private:
+                
+                AVFrame* frameData = nullptr;
 
-    /**
-     * @brief an abstraction on whatever backend is used for handling frames.
-     * 
-     */
-    class Frame{
-        private:
+            public:
+
+                /**
+                * @brief Construct an empty frame.
+                * 
+                */
+                Frame();
+                
+                Frame(AVFrame* dat);
+                
+                /**
+                * free all of the data in this Frame object.
+                */
+                void clear();
+                
+                /**
+                * clear the data of the frame when it is destroyed
+                */
+                ~Frame();
+
+                /**
+                * @brief Checks if the frame data is empty
+                * 
+                * @return true the frame data is empty
+                * @return false the frame data has contents
+                */
+                bool isEmpty() const;
+                
+                /**
+                * @brief retrieve a pointer to the internal data for the FFmpeg backend
+                * 
+                */
+                AVFrame* data() const;
+                
+                /**
+                * Convert a frame to an AVFrame without needing to call dataFF
+                * 
+                */
+                operator AVFrame*();
+                
+                /**
+                * directly assign an AVFrame to the Frame to set the value
+                */
+                Frame& operator=(AVFrame* x);
             
-
-        public:
-
-            /**
-             * @brief Construct an empty frame.
-             * 
-             */
-            Frame();
-            
-            /**
-             * free all of the data in this Frame object.
-             */
-            virtual void clear();
-            
-            /**
-             * clear the data of the frame when it is destroyed
-             */
-            virtual ~Frame();
-
-            /**
-             * @brief Checks if the frame data is empty
-             * 
-             * @return true the frame data is empty
-             * @return false the frame data has contents
-             */
-            virtual bool isEmpty() const;
-    };
+        };
+    }
 
 }
--- video/src/frame.cpp
+++ video/src/frame.cpp
@@ -1,17 +1,44 @@
 #include "frame.hpp"
 
-using namespace libtrainsim;
-
-Frame::Frame(){};
-
-bool Frame::isEmpty() const {
-    return true;
+libtrainsim::Video::Frame::~Frame() {
+    av_frame_free(&frameData);
+    av_free(frameData);
 }
 
-Frame::~Frame(){
-    clear();
+libtrainsim::Video::Frame::Frame(AVFrame* dat){
+    frameData = dat;
 }
 
-void Frame::clear(){
-    return;
+AVFrame* libtrainsim::Video::Frame::data() const{
+    return frameData;
+}
+
+libtrainsim::Video::Frame::Frame() {
+    frameData = av_frame_alloc();
+}
+
+libtrainsim::Video::Frame::operator AVFrame*(){
+    return frameData;
+}
+
+libtrainsim::Video::Frame& libtrainsim::Video::Frame::operator=(AVFrame* x){
+    
+    if (x == frameData){return *this;};
+    if(frameData != nullptr){
+        av_frame_free(&frameData);
+        av_free(frameData);
+    }
+    
+    frameData = x;
+    return *this;
+}
+
+bool libtrainsim::Video::Frame::isEmpty() const {
+    return frameData==nullptr;
+}
+
+void libtrainsim::Video::Frame::clear(){
+    av_frame_free(&frameData);
+    av_free(frameData);
+    frameData = av_frame_alloc();
 }
