109f63f304a152c334b6ff532cd53f5038263ae2
noasakurajin <dev@noasakurajin.de>
2022-08-12 14:40:53 +0200

    adding the initial module code for the snowFx module

---
+++ extras/snowFx/include/snowFx.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "imguiHandler.hpp"
+
+#include "helper.hpp"
+
+namespace libtrainsim{
+    namespace extras{
+        class snowFx{
+          private:
+            
+            
+          public:
+            snowFx();
+            
+            ~snowFx();
+            
+        };
+    }
+}
---
+++ extras/snowFx/src/snowFx.cpp
@@ -0,0 +1,9 @@
+#include "snowFx.hpp"
+
+libtrainsim::extras::snowFx::snowFx(){
+
+}
+
+libtrainsim::extras::snowFx::~snowFx() {
+    
+}
