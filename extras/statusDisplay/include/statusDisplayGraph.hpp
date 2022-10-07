dbeb7f09a15b775bec940483c5132cf1d62dcde7
noasakurajin <dev@noasakurajin.de>
2022-10-07 16:46:30 +0200

    adding documentation to statusDisplayGraph

--- extras/statusDisplay/include/statusDisplayGraph.hpp
+++ extras/statusDisplay/include/statusDisplayGraph.hpp
@@ -5,20 +5,56 @@
 
 namespace libtrainsim{
     namespace extras{
+        /**
+         * @brief an abstraction over the ImGui graphs for use with the statusDisplay
+         * This class provides an easy abstraction over the PlotLines function to
+         * handle all of the common graph display needs
+         * 
+         * @tparam VALUE_COUNT The number of samples this graph should have
+         */
         template<size_t VALUE_COUNT>
         class statusDisplayGraph{
         private:
+            /**
+             * @brief the internal array to store the data values that are displayed
+             */
             std::array<float, VALUE_COUNT> values;
             
+            /**
+             * @brief the name of the graph 
+             */
             std::string name;
+            
+            /**
+             * @brief the tooltip that should be shown when the graph is hovered
+             */
             std::string tooltip;
         public:
+            /**
+             * @brief create a new graph with a name and tooltip
+             */
             statusDisplayGraph(std::string graphName, std::string tooltipMessage);
             
+            /**
+             * @brief display the graph in a window
+             * 
+             * @param showLatest if true next to the name of the graph there will be the latest value
+             */
             void display(bool showLatest = true);
             
+            /**
+             * @brief append a value to the graph to be the latest value to be displayed
+             */
             void appendValue(float newValue);
+            
+            /**
+             * @brief get the name of the graph
+             */
             const std::string& getName();
+            
+            /**
+             * @brief get the latest value of the graph
+             */
             float getLatest();
         };
     }
