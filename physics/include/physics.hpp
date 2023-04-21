8868bbf13ab922df8456f49bcb5227471e1a62b7
Noa Sakurajin <14131388+noah1510@users.noreply.github.com>
2023-04-21 15:48:43 +0200

    cleanup of physics module
     This applies clang-format to the physics module.
     In addition to that all functions get the correct attributes and
     the source file is cleaned up a bit

--- physics/include/physics.hpp
+++ physics/include/physics.hpp
@@ -1,142 +1,154 @@
 #pragma once
 
-#include <shared_mutex>
-#include <mutex>
-#include <memory>
-#include <chrono>
-#include <cmath>
+#include "helper.hpp"
 
+#include "input_axis.hpp"
 #include "track_configuration.hpp"
 #include "types.hpp"
-#include "input_axis.hpp"
-#include "helper.hpp"
 
 #include "unit_system.hpp"
 
-namespace libtrainsim{
-    class LIBTRAINSIM_EXPORT_MACRO physics{
-        private:
+namespace libtrainsim {
 
-            std::shared_mutex mutex_data;
+    class LIBTRAINSIM_EXPORT_MACRO [[maybe_unused]] physics {
+      private:
+        std::shared_mutex mutex_data;
 
-            /**
-            * @brief The current acceleration of the train in ms^-2.
-            */
-            sakurajin::unit_system::acceleration current_acceleration;
+        /**
+         * @brief The current acceleration of the train in ms^-2.
+         */
+        sakurajin::unit_system::acceleration current_acceleration;
 
-            /**
-            * @brief The current velocity of the train in ms^-1.
-            */
-            sakurajin::unit_system::speed velocity;
+        /**
+         * @brief The current velocity of the train in ms^-1.
+         */
+        sakurajin::unit_system::speed velocity;
 
-            /**
-            * @brief The highest possible velocity of train.The value can be changed to any other value which makes sense.
-            * In the future this can be calculated by the resistance forces of Track and Train as well as the maximum of Trainpower.
-            */
-            const sakurajin::unit_system::speed MaxVelocity{85.0};
+        /**
+         * @brief The highest possible velocity of train.The value can be changed to any other value which makes sense.
+         * In the future this can be calculated by the resistance forces of Track and Train as well as the maximum of Trainpower.
+         */
+        const sakurajin::unit_system::speed MaxVelocity{85.0};
 
-            /**
-            * @brief The current location of the train at the track.
-            */
-            sakurajin::unit_system::length location;
+        /**
+         * @brief The current location of the train at the track.
+         */
+        sakurajin::unit_system::length location;
 
-            /**
-            * @brief The current Traction of the Train
-            */
-            sakurajin::unit_system::force currTraction;
+        /**
+         * @brief The current Traction of the Train
+         */
+        sakurajin::unit_system::force currTraction;
 
-            /**
-            * @brief The current Power of the Train. This variable is calculated and therefore not a const variable
-            */
-            sakurajin::unit_system::power currPower;
+        /**
+         * @brief The current Power of the Train. This variable is calculated and therefore not a const variable
+         */
+        sakurajin::unit_system::power currPower;
 
-            /**
-            * @brief The current Speedlevel of the Train. This variable is set by playerinput, the default value is 0.0
-            */
-            long double speedlevel = 0.0;
+        /**
+         * @brief The current Speedlevel of the Train. This variable is set by playerinput, the default value is 0.0
+         */
+        long double speedlevel = 0.0;
 
-            static std::chrono::time_point<std::chrono::high_resolution_clock> now(){
-                return std::chrono::high_resolution_clock::now();
-            }
+        const libtrainsim::core::Track config;
 
-            const libtrainsim::core::Track config;
+        std::chrono::time_point<std::chrono::high_resolution_clock> last_update;
 
-            std::chrono::time_point<std::chrono::high_resolution_clock> last_update;
+        std::shared_mutex mutex_error;
+        bool              hasError = true;
 
-            std::shared_mutex mutex_error;
-            bool hasError = true;
+        const bool autoTick;
 
-            const bool autoTick;
+        /**
+         * @brief This function calculates the maximum amount of the Force the Train can apply. This amount depends on the mass of the Train
+         * and the resistance force between train and track.
+         */
+        [[nodiscard]]
+        sakurajin::unit_system::force
+        calcMaxForce(sakurajin::unit_system::mass mass, sakurajin::unit_system::acceleration g, long double train_drag) const;
 
-            /**
-            * @brief This function calculates the maximum amount of the Force the Train can apply. This amount depends on the mass of the Train and the resistance force
-            * between train and track.
-            */
-            sakurajin::unit_system::force calcMaxForce(sakurajin::unit_system::mass mass, sakurajin::unit_system::acceleration g, long double train_drag)const;
+        /**
+         * @brief This function calculates the whole amount of the resistance forces and adds them together. Therefore this function uses
+         * the different types of drag coefficients from the traindata and the trackdata, Furthermore it uses the mass of the train as well
+         * as the location to get the train_drag coefficient correctly. This function is not implemented yet and it will return 0 by using
+         * it.
+         */
+        [[maybe_unused]] [[nodiscard]]
+        sakurajin::unit_system::force calcDrag();
 
-            /**
-            * @brief This function calculates the whole amount of the resistance forces and adds them together. Therefore this function uses the different types of drag coefficients from the traindata and the trackdata,
-            * Furthermore it uses the mass of the train as well as the location to get the train_drag coefficient correctly.
-            * This function is not implemented yet and it will return 0 by using it.
-            */
-            sakurajin::unit_system::force calcDrag();
-            
-            //true if the emergency break is activated.
-            //the train has to break full until it is stopped, after that it is allowed to accellerate again.
-            bool isEmergencyBreaking = false;
+        // true if the emergency break is activated.
+        // the train has to break full until it is stopped, after that it is allowed to accellerate again.
+        bool isEmergencyBreaking = false;
 
-        public:
+        /**
+         * @brief Calls the tick function if autoTick is enabled.
+         */
+        void doAutoTick();
+      public:
+        [[maybe_unused]] explicit physics(const libtrainsim::core::Track& conf, bool _autoTick = false);
 
-            physics(const libtrainsim::core::Track& conf, bool _autoTick = false);
+        ~physics();
 
-            ~physics();
+        [[maybe_unused]] [[nodiscard]]
+        bool isValid();
 
-            bool isValid();
+        /**
+         * @brief This function returns the current speedlevel of the train, which is set through the actions of a player.
+         * If autoTick is true, this function will automatically call tick before returning the value.
+         */
+        [[maybe_unused]]
+        void setSpeedlevel(const core::input_axis& slvl);
 
-            /**
-            * @brief This function returns the current speedlevel of the train, which is set through the actions of a player.
-            * If autoTick is true, this function will automatically call tick before returning the value.
-            */
-            void setSpeedlevel(core::input_axis slvl);
+        /**
+         * @brief This function returns the current velocity of the train. If autoTick is true, this function will automatically call tick
+         * before returning the value.
+         */
+        [[maybe_unused]] [[nodiscard]]
+        sakurajin::unit_system::speed getVelocity();
 
-            /**
-            * @brief This function returns the current velocity of the train. If autoTick is true, this function will automatically call tick before returning the value.
-            */
-            sakurajin::unit_system::speed getVelocity();
+        /**
+         * @brief This function returns the current location of the train along the track.
+         * If autoTick is true, this function will automatically call tick before returning the value.
+         */
+        [[maybe_unused]] [[nodiscard]]
+        sakurajin::unit_system::length getLocation();
 
-            /**
-            * @brief This function returns the current location of the train along the track.
-            * If autoTick is true, this function will automatically call tick before returning the value.
-            */
-            sakurajin::unit_system::length getLocation();
+        /**
+         * @brief This function returns the current acceleration of the train. If autoTick is true, this function will automatically call
+         * tick before returning the value.
+         */
+        [[maybe_unused]] [[nodiscard]]
+        sakurajin::unit_system::acceleration getAcceleration();
 
-            /**
-            * @brief This function returns the current acceleration of the train. If autoTick is true, this function will automatically call tick before returning the value.
-            */
-            sakurajin::unit_system::acceleration getAcceleration();
+        /**
+         * @brief This function returns the current traction of the train. If autoTick is true, this function will automatically call tick
+         * before returning the value.
+         */
+        [[maybe_unused]] [[nodiscard]]
+        sakurajin::unit_system::force getTraction();
 
-            /**
-            * @brief This function returns the current traction of the train. If autoTick is true, this function will automatically call tick before returning the value.
-            */
-            sakurajin::unit_system::force getTraction();
+        /**
+         * @brief This function returns the current power of the train. If autoTick is true, this function will automatically call tick
+         * before returning the value.
+         */
+        [[maybe_unused]] [[nodiscard]]
+        sakurajin::unit_system::power getCurrPower();
 
-            /**
-            * @brief This function returns the current power of the train. If autoTick is true, this function will automatically call tick before returning the value.
-            */
-            sakurajin::unit_system::power getCurrPower();
+        /**
+         * @brief activate the emergency break
+         *
+         * This causes the train to break until it comes to a stop.
+         */
+        [[maybe_unused]]
+        void emergencyBreak();
 
-            /**
-             * @brief activate the emergency break
-             * 
-             * This causes the train to break until it comes to a stop.
-             */
-            void emergencyBreak();
-            
-            bool reachedEnd();
+        [[maybe_unused]] [[nodiscard]]
+        bool reachedEnd();
 
-            /**
-            * @brief This Function does the whole mathematics behind the physics. It calculates the current values of location, velocity, acceleration, traction and trainpower for every tick the simulation is running.
-            */
-            void tick();
+        /**
+         * @brief This Function does the whole mathematics behind the physics. It calculates the current values of location, velocity,
+         * acceleration, traction and trainpower for every tick the simulation is running.
+         */
+        void tick();
     };
-}
+} // namespace libtrainsim
--- physics/src/physics.cpp
+++ physics/src/physics.cpp
@@ -1,153 +1,151 @@
 #include "physics.hpp"
 
-using namespace libtrainsim;
-using namespace libtrainsim::core;
-
 using namespace sakurajin::unit_system::literals;
 using namespace sakurajin::unit_system;
 
-physics::physics(const Track& conf, bool _autoTick):config(conf),autoTick(_autoTick){
+libtrainsim::physics::physics(const libtrainsim::core::Track& conf, bool _autoTick)
+    : config(conf),
+      autoTick(_autoTick) {
 
     std::scoped_lock<std::shared_mutex> lock1(mutex_data);
-    velocity = 0.0_mps;
-    location = config.firstLocation();
+
+    velocity             = 0.0_mps;
+    location             = config.firstLocation();
     current_acceleration = 0.0_mps2;
-
-
-
-    last_update = Helper::now();
+    last_update          = libtrainsim::core::Helper::now();
 
     std::scoped_lock<std::shared_mutex> lock2(mutex_error);
     hasError = false;
-    return;
-};
+}
 
-physics::~physics(){
+libtrainsim::physics::~physics() = default;
 
-};
+void libtrainsim::physics::doAutoTick() {
+    if (autoTick) {
+        tick();
+    }
+}
 
-void physics::emergencyBreak(){
+void libtrainsim::physics::emergencyBreak() {
     std::scoped_lock<std::shared_mutex> lock(mutex_data);
     isEmergencyBreaking = true;
 }
 
-speed physics::getVelocity(){
-    if(autoTick){tick();};
+speed libtrainsim::physics::getVelocity() {
+    doAutoTick();
     std::shared_lock<std::shared_mutex> lock(mutex_data);
     return velocity;
 }
 
-length physics::getLocation(){
-    if(autoTick){tick();};
+length libtrainsim::physics::getLocation() {
+    doAutoTick();
     std::shared_lock<std::shared_mutex> lock(mutex_data);
     return location;
 }
 
-acceleration physics::getAcceleration(){
-    if(autoTick){tick();};
-      std::shared_lock<std::shared_mutex> lock(mutex_data);
-      return current_acceleration;
+acceleration libtrainsim::physics::getAcceleration() {
+    doAutoTick();
+    std::shared_lock<std::shared_mutex> lock(mutex_data);
+    return current_acceleration;
 }
 
-void physics::setSpeedlevel(core::input_axis slvl){
-    tick();
+void libtrainsim::physics::setSpeedlevel(const core::input_axis& slvl) {
+    doAutoTick();
     std::scoped_lock<std::shared_mutex> lock(mutex_data);
     speedlevel = slvl.get();
 }
 
-force physics::getTraction(){
-    if(autoTick){tick();};
+force libtrainsim::physics::getTraction() {
+    doAutoTick();
     std::shared_lock<std::shared_mutex> lock(mutex_data);
     return currTraction;
 }
 
-power physics::getCurrPower(){
-  if(autoTick){tick();};
-  std::shared_lock<std::shared_mutex> lock(mutex_data);
-  return currPower;
+power libtrainsim::physics::getCurrPower() {
+    doAutoTick();
+    std::shared_lock<std::shared_mutex> lock(mutex_data);
+    return currPower;
 }
 
-force physics::calcMaxForce(mass mass, acceleration g, long double train_drag)const{
-    force maxforce = mass*g*train_drag;
+force libtrainsim::physics::calcMaxForce(mass mass, acceleration g, long double train_drag) const {
+    force maxforce = mass * g * train_drag;
     return maxforce;
 }
 
-force physics::calcDrag(){
-  return 0_N;
+force libtrainsim::physics::calcDrag() {
+    return 0_N;
 }
 
-bool physics::isValid(){
+bool libtrainsim::physics::isValid() {
     std::shared_lock<std::shared_mutex> lock(mutex_error);
     return !hasError;
 }
 
-bool physics::reachedEnd(){
-    if(autoTick){tick();};
+bool libtrainsim::physics::reachedEnd() {
+    doAutoTick();
     std::scoped_lock<std::shared_mutex> lock(mutex_data);
     return std::abs((location - config.lastLocation()).value) < 0.1;
 }
 
-void physics::tick(){
+void libtrainsim::physics::tick() {
 
     std::scoped_lock<std::shared_mutex> lock(mutex_data);
 
-    auto new_time = now();
+    auto new_time = libtrainsim::core::Helper::now();
 
     time_si dt = unit_cast(new_time - last_update);
 
-    //all Variables needed to caclulate the physics
+    // all Variables needed to caclulate the physics
     sakurajin::unit_system::power MaxPower;
     sakurajin::unit_system::force MaxForce;
-    sakurajin::unit_system::mass mass;
-    long double air_drag = 0.0;
-    long double train_drag = 0.0;
+    sakurajin::unit_system::mass  mass;
+    long double                   air_drag   = 0.0;
+    long double                   train_drag = 0.0;
 
-    //defining the needed variables
-    mass = config.train().getMass();
+    // defining the needed variables
+    mass       = config.train().getMass();
     train_drag = config.train().getTrackDrag();
 
-    MaxForce = calcMaxForce(mass,1_G,train_drag);
+    MaxForce = calcMaxForce(mass, 1_G, train_drag);
     MaxPower = config.train().getMaxPower();
 
-    if(isEmergencyBreaking){
-      speedlevel = -1.0;
-      if(velocity < 0.007_mps){
-        isEmergencyBreaking = false;
-      }
-    }
-    
-    currPower = speedlevel*MaxPower;
-
-    //Handling the different possibilities for Speedlevel
-    //only calculating the current Force
-    if (speedlevel > 0.007){
-      if (abs(velocity) < 0.007_mps){
-        currTraction = MaxForce;
-      }else{
-        currTraction = currPower/velocity;
-      }
-
-      if (currTraction >  MaxForce) {
-        currTraction = MaxForce;
-      }
-    }else if (speedlevel < -0.007){
-      currTraction = speedlevel*MaxForce;
-    }else {
-        currTraction = 0_N;
-        if (velocity > 0.0_mps){
-          currTraction = currPower/velocity;
+    if (isEmergencyBreaking) {
+        speedlevel = -1.0;
+        if (velocity < 0.007_mps) {
+            isEmergencyBreaking = false;
         }
     }
 
-    //calculating parameters of movement by current Traction
-    current_acceleration = currTraction/mass;
+    currPower = speedlevel * MaxPower;
+
+    // Handling the different possibilities for Speedlevel
+    // only calculating the current Force
+    if (speedlevel > 0.007) {
+        if (abs(velocity) < 0.007_mps) {
+            currTraction = MaxForce;
+        } else {
+            currTraction = currPower / velocity;
+        }
+
+        if (currTraction > MaxForce) {
+            currTraction = MaxForce;
+        }
+    } else if (speedlevel < -0.007) {
+        currTraction = speedlevel * MaxForce;
+    } else {
+        currTraction = 0_N;
+        if (velocity > 0.0_mps) {
+            currTraction = currPower / velocity;
+        }
+    }
+
+    // calculating parameters of movement by current Traction
+    current_acceleration = currTraction / mass;
     velocity += current_acceleration * dt;
     location += velocity * dt + 0.5 * (current_acceleration * dt * dt);
 
-    location = clamp(location, config.firstLocation(),config.lastLocation());
-    velocity = clamp(velocity,0_mps,MaxVelocity);
+    location = clamp(location, config.firstLocation(), config.lastLocation());
+    velocity = clamp(velocity, 0_mps, MaxVelocity);
 
     last_update = new_time;
-
-    return;
 }
